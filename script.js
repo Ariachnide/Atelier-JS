726ebad2c3a03b9b6b22c3c6671951f155d6380d
Aria <rb.guerre@gmail.com>
2023-06-21 15:35:31 +0200

    put switch in function

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const firstValue = parseInt(prompt("Veuillez saisir une première valeur :"));
-const operator = prompt("Veuillez choisir un opérateur :");
-const secondValue = parseInt(prompt("Veuillez saisir une deuxième valeur :"));
+const userFirstValue = parseInt(prompt("Veuillez saisir une première valeur :"));
+const userOperator = prompt("Veuillez choisir un opérateur :");
+const userSecondValue = parseInt(prompt("Veuillez saisir une deuxième valeur :"));
 
-if (isNaN(firstValue) || isNaN(secondValue)) {
-    console.error("Toutes les valeurs saisies ne sont pas des nombres !");
-} else {
+function compute(firstValue, operator, secondValue) {
     switch(operator) {
         case "+":
             console.log(firstValue + secondValue);
-            break;
+            return;
         case "-":
             console.log(firstValue - secondValue);
-            break;
+            return;
         case "*":
             console.log(firstValue * secondValue);
-            break;
+            return;
         case "/":
             console.log(firstValue / secondValue);
-            break;
+            return;
         default:
             console.error("L'opérateur saisi est invalide.");
-            break;
+            return;
     }
+}
+
+if (isNaN(userFirstValue) || isNaN(userSecondValue)) {
+    console.error("Toutes les valeurs saisies ne sont pas des nombres !");
+} else {
+    compute(userFirstValue, userOperator, userSecondValue);
 }
